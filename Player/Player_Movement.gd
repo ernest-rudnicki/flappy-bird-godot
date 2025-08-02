ab2eac62ca1ba3cef4499def71ab59040df48a4a
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2025-08-02 19:35:55 +0200

    add jumping

--- Player/Player_Movement.gd
+++ Player/Player_Movement.gd
@@ -2,8 +2,11 @@ extends CharacterBody3D
 
 
 @export var gravity: float = 9.81;
+@export var jumpForce: float = 20;
+
 
 func _physics_process(delta: float) -> void:
-	velocity.y -= gravity * delta;
-	
-	move_and_slide()
+	if Input.is_action_pressed("jump"):
+		velocity.y = jumpForce
+	else:
+		velocity.y -= gravity * delta
