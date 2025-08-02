cdf4843fc62eb2df196a193ff68681746ba829d0
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2025-08-02 19:36:42 +0200

    Add treshold for jump force

--- Player/Player_Movement.gd
+++ Player/Player_Movement.gd
@@ -3,10 +3,16 @@ extends CharacterBody3D
 
 @export var gravity: float = 9.81;
 @export var jumpForce: float = 20;
+@export var canJumpForceTreshold: float = -10;
 
+var cannotJump = false;
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("jump"):
+	if Input.is_action_pressed("jump") and not cannotJump:
 		velocity.y = jumpForce
+		cannotJump = true
 	else:
 		velocity.y -= gravity * delta
+		
+	if velocity.y < canJumpForceTreshold:
+		cannotJump = false
