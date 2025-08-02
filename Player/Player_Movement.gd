c6c4816b069817a89df8ab555411e0722baafa4f
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2025-08-02 19:42:14 +0200

    wait for first jump

--- Player/Player_Movement.gd
+++ Player/Player_Movement.gd
@@ -6,12 +6,14 @@ extends CharacterBody3D
 @export var canJumpForceTreshold: float = -10;
 
 var cannotJump = false;
+var hadFirstJump = false;
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("jump") and not cannotJump:
 		velocity.y = jumpForce
 		cannotJump = true
-	else:
+		hadFirstJump = true
+	elif hadFirstJump:
 		velocity.y -= gravity * delta
 		
 	if velocity.y < canJumpForceTreshold:
