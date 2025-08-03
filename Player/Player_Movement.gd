fdda2947a3d2cdfad8cd7a6925a19f621db5b03f
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2025-08-03 15:28:28 +0200

    restrict movement from the top

--- Player/Player_Movement.gd
+++ Player/Player_Movement.gd
@@ -1,22 +1,32 @@
 extends CharacterBody3D
 
+@export var gravity: float = 40.0
+@export var jumpForce: float = 20.0
+@export var canJumpForceTreshold: float = -10.0
 
-@export var gravity: float = 40;
-@export var jumpForce: float = 20;
-@export var canJumpForceTreshold: float = -10;
+@export var minYValue: float = -15.0
+@export var maxYValue: float = 10.0
 
-var cannotJump = false;
+var isStillJumping = false;
 var hadFirstJump = false;
 
-func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("jump") and not cannotJump:
+func _physics_process(delta: float) -> void:	
+	if Input.is_action_pressed("jump") and canJump():
 		velocity.y = jumpForce
-		cannotJump = true
+		isStillJumping = true
 		hadFirstJump = true
 	elif hadFirstJump:
 		velocity.y -= gravity * delta
 		
 	if velocity.y < canJumpForceTreshold:
-		cannotJump = false
+		isStillJumping = false
 		
 	move_and_slide()
+	
+	global_position.y = clamp(global_position.y, minYValue, maxYValue)
+	
+	if global_position.y == maxYValue:
+		velocity.y = 0.0
+	
+func canJump():
+	return not isStillJumping
