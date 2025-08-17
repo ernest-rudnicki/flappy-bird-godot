9e39258eed1f409ffb80d717db3ad150b2ecb4c0
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2025-08-17 11:37:33 +0200

    remove passed obstacles

--- Obstacles/ObstacleManager.gd
+++ Obstacles/ObstacleManager.gd
@@ -4,25 +4,35 @@ extends Node3D
 @export var obstacle_scene: PackedScene
 @export var speed: float = 5.0;
 
-
 @export var gap_size: float = 4.0
 @export var min_center: float = -4.0
 @export var max_center: float = 4.0
+@export var spawn_interval: float = 2.0
 
 var obstacles: Array[Node3D] = []
 var rng = RandomNumberGenerator.new()
 
 func _ready() -> void:
-	create_obstacle()
+		create_obstacle()
 
+func _on_timer_timeout() -> void:
+	create_obstacle()
 
 func _process(delta: float) -> void:
 	move_all_obstacles(delta)
 	
 func move_all_obstacles(delta: float) -> void:
+	var to_remove: Array = []
+	
 	for obstacle in obstacles:
 		obstacle.global_position.x -= speed * delta
-
+		
+		if obstacle.global_position.x < -15.0:
+			obstacle.queue_free()
+			to_remove.append(obstacle)
+			
+	for removed in to_remove:
+		obstacles.erase(removed)
 	
 func create_obstacle() -> void:
 	var upper_obstacle = instantiate_obstacle()
